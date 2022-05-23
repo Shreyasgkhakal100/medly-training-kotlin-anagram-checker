ea0f17511e482fa1f65d1f5a2b5106eaf974a07b
Shreyas Khakal <shreyasgkhakal100@gmail.com>
2022-05-23 14:12:56 +0530

    Add test cases for kotlin-anagram-checker assignment

--- anagram-checker/src/AnagramChecker.kt
+++ anagram-checker/src/AnagramChecker.kt
@@ -2,9 +2,11 @@ class AnagramChecker {
     fun match(word1: String, word2: String): Boolean {
         val counter = IntArray(26) { 0 }
         if (word1.length != word2.length) return false
+//        var string1 = word1.lowercase()
+//        var string2 = word2.lowercase()
         (word1.indices).forEach { index ->
-            ++counter[word1[index] - 'a']
-            --counter[word2[index] - 'a']
+            ++counter[word1.lowercase()[index] - 'a']
+            --counter[word2.lowercase()[index] - 'a']
         }
         return !counter.any { it != 0 }
     }
--- anagram-checker/test/AnagramCheckerTest.kt
+++ anagram-checker/test/AnagramCheckerTest.kt
@@ -1,10 +1,31 @@
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
+import javax.print.DocFlavor.STRING
 
 class AnagramCheckerTest : StringSpec() {
     init {
-        "is 'a' an anagram of 'a'" {
+        "should return true if two strings are anagrams with same characters" {
             AnagramChecker().match("a", "a").shouldBeTrue()
+        }
+        "should return false if two strings are not anagrams with different characters" {
+            AnagramChecker().match("a", "b").shouldBeFalse()
+        }
+        "should return true if two strings are anagrams with same characters but different sequence" {
+            AnagramChecker().match("ab", "ba").shouldBeTrue()
+        }
+        "should return true if strings are anagrams with uppercase characters" {
+            AnagramChecker().match("A", "A").shouldBeTrue()
+        }
+        "should return true if strings are anagrams with mixed case characters" {
+            AnagramChecker().match("Ab", "Ba").shouldBeTrue()
+        }
+        "should return false if strings are not anagrams with mixed case characters" {
+            AnagramChecker().match("Ab", "Bb").shouldBeFalse()
+        }
+        "should return  false if strings are not anagrams with same character but of different length" {
+            AnagramChecker().match("a", "aa").shouldBeFalse()
         }
     }
 }
